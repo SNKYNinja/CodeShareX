68ada1a38b8acf72e01603eb3ff7043636be5e31
Aditya Chaudhary <96983394+SNKYNinja@users.noreply.github.com>
2024-06-13 22:39:54 +0530

    feat: Add site inital metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { Metadata } from "next";
 
 const defaultUrl = process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}`
     : "http://localhost:3000";
 
-export const metadata = {
+export const metadata: Metadata = {
     metadataBase: new URL(defaultUrl),
-    title: "Next.js and Supabase Starter Kit",
-    description: "The fastest way to build apps with Next.js and Supabase",
+    title: "CodeShareX",
+    description: "Your go to tool for managing code snippets",
 };
 
 export default function RootLayout({
